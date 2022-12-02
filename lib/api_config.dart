6ff174eb4eeaee959d6cd4200b03c4d76b234a14
George Zhang <georgezy@amazon.com>
2022-12-02 11:38:23 -0800

    Add apiUrl format to README and api_config.dart

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -4,6 +4,7 @@
  */
 
 class ApiConfig {
+  // Format: https://<api-id>.execute-api.<aws-region-id>.amazonaws.com/Prod/
   static String get apiUrl => ""; // API url goes here
   static String get region => ""; // Add region here
 }
