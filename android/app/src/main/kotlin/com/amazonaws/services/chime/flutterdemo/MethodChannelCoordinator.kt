f1286134c034fcf8e84a48a517ac42ba3dfb9b8a
Kautsar Al Bana <kautsar.al.bana@kineticastudios.com>
2023-07-07 09:00:21 +0700

    Update Attendee

--- android/app/src/main/kotlin/com/amazonaws/services/chime/flutterdemo/MethodChannelCoordinator.kt
+++ android/app/src/main/kotlin/com/amazonaws/services/chime/flutterdemo/MethodChannelCoordinator.kt
@@ -2,7 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: MIT-0
  */
- 
+
 package com.amazonaws.services.chime.flutterdemo
 
 import com.amazonaws.services.chime.sdk.meetings.device.MediaDevice
